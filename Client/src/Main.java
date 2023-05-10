1722beb51388247dd5b2a6fd1b3e34c048b0a5ed
[ sora. ] <57730505+GoodZerg@users.noreply.github.com>
2023-05-10 23:06:27 +0300

    test API Java TCP

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,5 +1,26 @@
+import java.net.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String serverName = "localhost";
+        int port = 2222;
+        try {
+            System.out.println("Подключение к " + serverName + " на порт " + port);
+            Socket client = new Socket(serverName, port);
+
+            System.out.println("Просто подключается к " + client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+
+            out.writeUTF("Привет из " + client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+
+            System.out.println("Сервер ответил " + in.readUTF());
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int port = 2222;
+        try {
+            Thread t = new Server(port);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
